a173a29719f0e260cc92901b632f114e63e8efdc
Emiko Miura <emiura@catmktg.com>
2017-01-04 14:08:33 +0900

    Create New

---
+++ GetSchedule/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace GetSchedule
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGetSchedule = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.TextBox();
+            this.dateFrom = new System.Windows.Forms.DateTimePicker();
+            this.dateTo = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "日付";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(237, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "～";
+            // 
+            // btnGetSchedule
+            // 
+            this.btnGetSchedule.Location = new System.Drawing.Point(439, 44);
+            this.btnGetSchedule.Name = "btnGetSchedule";
+            this.btnGetSchedule.Size = new System.Drawing.Size(79, 36);
+            this.btnGetSchedule.TabIndex = 4;
+            this.btnGetSchedule.Text = "出力";
+            this.btnGetSchedule.UseVisualStyleBackColor = true;
+            this.btnGetSchedule.Click += new System.EventHandler(this.btnGetSchedule_Click);
+            // 
+            // output
+            // 
+            this.output.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.output.Location = new System.Drawing.Point(58, 103);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.output.Size = new System.Drawing.Size(460, 332);
+            this.output.TabIndex = 5;
+            this.output.TabStop = false;
+            // 
+            // dateFrom
+            // 
+            this.dateFrom.CustomFormat = "";
+            this.dateFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateFrom.Location = new System.Drawing.Point(102, 51);
+            this.dateFrom.Name = "dateFrom";
+            this.dateFrom.Size = new System.Drawing.Size(108, 19);
+            this.dateFrom.TabIndex = 1;
+            // 
+            // dateTo
+            // 
+            this.dateTo.CustomFormat = "";
+            this.dateTo.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTo.Location = new System.Drawing.Point(281, 51);
+            this.dateTo.Name = "dateTo";
+            this.dateTo.Size = new System.Drawing.Size(108, 19);
+            this.dateTo.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 481);
+            this.Controls.Add(this.dateTo);
+            this.Controls.Add(this.dateFrom);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.btnGetSchedule);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "日報出力";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGetSchedule;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.DateTimePicker dateFrom;
+        private System.Windows.Forms.DateTimePicker dateTo;
+    }
+}
+
---
+++ GetSchedule/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Outlook;
+
+namespace GetSchedule
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetSchedule_Click(object sender, EventArgs e)
+        {
+            output.Clear();
+
+            string fdate = dateFrom.Value.Date.ToShortDateString();
+            string tdate = dateTo.Value.Date.ToShortDateString();
+
+            // 日付チェック
+            if (fdate.CompareTo(tdate).Equals(1))
+            {
+                MessageBox.Show("To日付はFrom日付より新しい日付でなければなりません。");
+                return;
+            } 
+
+            // スケジュール取得
+            Microsoft.Office.Interop.Outlook.Application outlook = new Microsoft.Office.Interop.Outlook.Application();
+            NameSpace ns = outlook.GetNamespace("MAPI");
+            MAPIFolder oFolder = ns.GetDefaultFolder(OlDefaultFolders.olFolderCalendar);
+            //output.Text += oFolder.Name + "\r\n\r\n---\r\n";
+            output.Text += "作業日報" + "\r\n\r\n-----\r\n";
+
+            Items oItems = oFolder.Items;
+            AppointmentItem oAppoint = oItems.GetFirst();
+
+            string startDate;
+            string endDate;
+            bool schedule = false;
+
+            while (oAppoint != null)
+            {
+                startDate = oAppoint.Start.ToShortDateString();
+                endDate = oAppoint.End.ToShortDateString();
+
+                // 指定した日付の範囲の予定表を抽出
+                if ((startDate.CompareTo(fdate).Equals(0) || startDate.CompareTo(fdate).Equals(1))
+                    && (tdate.CompareTo(endDate).Equals(0) || tdate.CompareTo(endDate).Equals(1)))
+                {
+                    output.Text += "件名    ：  " + oAppoint.Subject + "\r\n";
+                    output.Text += "開始時刻：  " + oAppoint.Start.ToString("yyyy/MM/dd hh:mm:ss") + "\r\n";
+                    output.Text += "終了時刻：  " + oAppoint.End.ToString("yyyy/MM/dd hh:mm:ss") + "\r\n";
+                    output.Text += "内容　　：　\r\n" + oAppoint.Body + "\r\n";
+                    output.Text += "\r\n-----\r\n";
+                    schedule = true;
+                }
+                oAppoint = oItems.GetNext();
+            }
+
+            // スケジュール登録がない場合
+            if (!schedule)
+            {
+                output.Text += "指定した日付の日報はありません。";
+            } 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+    }
+}
